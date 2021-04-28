3890149dc0389bae799bec9ea00f9742134f9fdc
legend <kygx.legend@gmail.com>
2021-04-28 20:09:29 +0800

    update graphx

--- graphx/src/main/scala/PPR.scala
+++ graphx/src/main/scala/PPR.scala
@@ -1,8 +1,16 @@
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.graphx.GraphLoader
 
 object PPR {
   def main(args: Array[String]) {
     val spark = SparkSession.builder.appName("PPR").getOrCreate()
+
+    // Load the edges as a graph
+    val graph = GraphLoader.edgeListFile(sc, "data/graphx/followers.txt")
+    // Select source vertices
+    val vertices: Array[VertexId] = Array((), (), ())
+    val ranks = graph.staticParallelPersonalizedPageRank(vertices, 10, 0.2)
+
     spark.stop()
   }
 }
